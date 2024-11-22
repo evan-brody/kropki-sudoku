88a76bfca4bbca2d1c579612c540dda84d3c534e
Evan Brody <brody_e@outlook.com>
2024-11-22 18:02:45 -0500

    cleanup

--- kropki.py
+++ kropki.py
@@ -121,7 +121,6 @@ def count_valid_values(board, i, j) -> int:
 
 def select_square_to_fill(board):
     pass
-    pass
 
 def solve_kropki_board(board):
     if all(board.nums): return board # Complete assignment. Must be valid
